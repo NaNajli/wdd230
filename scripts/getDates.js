f30c305cd3a5f92c8e80e33f085a6526285280e2
NaNajli <nadurodriguez2@gmail.com>
2024-01-18 16:00:48 -0300

    Home Page Enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,26 @@
-let oLastModif = document.lastModified;
+let oLastModif = "Last Modified :" + document.lastModified;
 document.getElementById("lastModified").innerHTML = oLastModif;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        main.style.background = "#F5D6A3";
+        main.style.color = "#3D405B";
+        modeButton.textContent = "🔆";
+    } else {
+        main.style.background = "#3D405B";
+        main.style.color = "#F5D6A3";
+        modeButton.textContent = "🕶️";
+    }
+});
