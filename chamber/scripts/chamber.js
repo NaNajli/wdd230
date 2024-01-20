fb4f89ae1588c119915b562d5db17093e88a7efe
NaNajli <nadurodriguez2@gmail.com>
2024-01-20 02:35:02 -0300

    The Chamber of Commerce Home Page

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,10 @@
+let oLastModif = "Last Modified :" + document.lastModified;
+document.getElementById("lastModified").innerHTML = oLastModif;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.home');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
