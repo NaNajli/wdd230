4de0eeef566750efe232908f92e5a084552a5c2b
NaNajli <nadurodriguez2@gmail.com>
2024-02-08 19:49:45 -0300

    Weather API

---
+++ week05/scripts/weather.js
@@ -0,0 +1,36 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+//const mykey = "96785fdaafd1d409d618eab3ed7a191d";
+//const lat = "49.75579701018483";
+//const lon = "6.639421453418779";
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=96785fdaafd1d409d618eab3ed7a191d&units=imperial'
+
+async function apiFetch(url) {
+    const response = await fetch(url);
+    try {
+        if (response.ok) {
+            const data = await response.json();
+            console.table(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch(url);
+
+function displayResults(data) {
+
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/wn/10d@2x.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+    captionDesc.textContent = `${desc}`;
+}
